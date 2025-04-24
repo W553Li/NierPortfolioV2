2a9b0a0b205dd8671a56e80dafd8e3ab21951c4d
William <w553li@uwaterloo.ca>
2025-04-23 20:11:23 -0600

    April 23 More descriptions, icons, and pictures

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ButtonPanel from '../components/ButtonPanel'
 import Panel from '../components/Panel'
 import PageTemplate from './PageTemplate'
 
 export default function About() {
   const [activePanel, setActivePanel] = useState(null)
+  const [activeButtonPanel, setActiveButtonPanel] = useState(null)
+
+  const selections = {
+    // "Web Development": ["ASTunnel", "BudgetTracker", "ImageSearcher", "ToDoList", "Snake"],
+    // "Games": ["Candlelight"]
+    "Skills": ["Programming Languages", "Technologies", "Developer Tools", "Certifications"],
+  }
 
   const panelContent = {
     "Me": [
@@ -15,45 +23,129 @@ export default function About() {
       `Github: <a href="https://github.com/W553Li" target="_blank" rel="noopener noreferrer">github.com/W553Li</a>`,
       // `Resume: <a href="/path-to-your-resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>`
     ].join('\n'),
-    "Skills": [
-      // "Web Development: HTML, CSS, JavaScript, React, PHP",
-      // "Programming Languages:",
-      // "• Python",
-      // "• C/C++",
-      // "• HTML",
-      // "• CSS",
-      // "• JavaScript",
-      // "• PHP",
-      // "• MySQL",
-      // "• MATLAB",
-      // "",
-      // "Frameworks & Tools:",
-      // "• React",
-      // "• Node.js",
-      // "• Git"
-      // "Programming Languages: Python, C/C++, Java, JavaScript, HTML, CSS, PHP, MySQL, MATLAB", "",
-      `Programming Languages: <img src="/icons/icons8-python-20.svg"/> Python, <img src="/icons/icons8-c++.svg"/> C/C++, <img src="/icons/icons8-java.svg"/> Java, <img src="/icons/icons8-javascript.svg"/> JavaScript, <img src="/icons/icons8-html.svg"/> HTML, <img src="/icons/icons8-css.svg"/> CSS, <img src="/icons/icons8-php.svg"/> PHP, <img src="/icons/icons8-mysql.svg"/> MySQL, <img src="/icons/icons8-matlab.svg"/> MATLAB`, "",
-      "Technologies: ",
-        "• AI and Machine Learning: TensorFlow, Scikit-Learn, OpenCV",
-        "• Web Development: Supabase, Firebase, Vite, Flask, ReactJS, Node.js, Bootstrap, Tailwind CSS",
-        "",
-      "Developer Tools: Git, Jira, MS Azure, MS PowerBI, VS Code, Intellij IDEA, PhpStorm, MySQL Workbench", "",
-      "Certifications: Microsoft Azure Fundamentals, Microsoft Azure AI Fundamentals"
-    ].join('\n')
+    "Programming Languages": [
+      `• <img src="/icons/python.svg" style="max-width: 20px;"/> Python`, 
+      `• <img src="/icons/c++.svg" style="max-width: 20px;"/> C/C++`, 
+      `• <img src="/icons/java.svg" style="max-width: 20px;"/> Java`, 
+      `• <img src="/icons/javascript.svg" style="max-width: 20px;"/> JavaScript`, 
+      `• <img src="/icons/html.svg" style="max-width: 20px;"/> HTML`, 
+      `• <img src="/icons/css.svg" style="max-width: 20px;"/> CSS`, 
+      `• <img src="/icons/php.svg" style="max-width: 20px;"/> PHP`, 
+      `• <img src="/icons/mysql.svg" style="max-width: 20px;"/> MySQL`, 
+      `• <img src="/icons/matlab.svg" style="max-width: 20px;"/> MATLAB`,
+    ].join('\n'),
+    "Technologies": [
+      `AI and Machine Learning:`, 
+        `• <img src="/icons/Keras.svg" style="max-width: 20px;"/> Keras`, 
+        `• <img src="/icons/tensorflow.svg" style="max-width: 20px;"/> TensorFlow`, 
+        `• <img src="/icons/scikit-learn.svg" style="max-width: 20px;"/> Scikit-Learn`, 
+        `• <img src="/icons/OpenCV.svg" style="max-width: 20px;"/> OpenCV`,
+      "",
+      `Web Development:`, 
+        `• <img src="/icons/supabase.svg" style="max-width: 20px;"/> Supabase`, 
+        `• <img src="/icons/Firebase.svg" style="max-width: 20px;"/> Firebase`, 
+        `• <img src="/icons/Vite.svg" style="max-width: 20px;"/> Vite`, 
+        `• <img src="/icons/Flask.svg" style="max-width: 20px;"/> Flask`, 
+        `• <img src="/icons/React.svg" style="max-width: 20px;"/> ReactJS`, 
+        `• <img src="/icons/Node.svg" style="max-width: 20px;"/> Node.js`, 
+        `• <img src="/icons/Bootstrap.svg" style="max-width: 20px;"/> Bootstrap`, 
+        `• <img src="/icons/Tailwind.svg" style="max-width: 20px;"/> Tailwind CSS`, 
+        `• <img src="/icons/Threejs.svg" style="max-width: 20px;"/> ThreeJS`,
+      "",
+      `Data and Automation:`, 
+        `• <img src="/icons/NumPy.svg" style="max-width: 20px;"/> Numpy`, 
+        `• <img src="/icons/Pandas.svg" style="max-width: 20px;"/> Pandas`, 
+        `• <img src="/icons/beautifulsoup.svg" style="max-width: 20px;"/> BeautifulSoup`, 
+        `• <img src="/icons/Selenium.svg" style="max-width: 20px;"/> Selenium`,
+      "",
+    ].join('\n'),
+    "Developer Tools": [
+      `• <img src="/icons/GitHub.svg" style="max-width: 20px;"/> Git`, 
+      `• <img src="/icons/Jira.svg" style="max-width: 20px;"/> Jira`, 
+      `• <img src="/icons/Azure.svg" style="max-width: 20px;"/> MS Azure`, 
+      `• <img src="/icons/powerbi.svg" style="max-width: 20px;"/> MS PowerBI`, 
+      `• <img src="/icons/vscode.svg" style="max-width: 20px;"/> VS Code`, 
+      `• <img src="/icons/IntelliJ.svg" style="max-width: 20px;"/> Intellij IDEA`, 
+      `• <img src="/icons/PhpStorm.svg" style="max-width: 20px;"/> PhpStorm`, 
+      `• <img src="/icons/mysql.svg" style="max-width: 20px;"/> MySQL Workbench`,
+    ].join('\n'),
+    "Certifications": [
+      // "Microsoft Azure Fundamentals, Microsoft Azure AI Fundamentals"
+      `• <img src="/icons/Azure.svg" style="max-width: 20px;"/> <a href="https://www.credly.com/badges/01c5fcec-294c-4f59-bc54-454277ef7a21" target="_blank" rel="noopener noreferrer">Microsoft Azure Fundamentals</a>`,
+      `• <img src="/icons/Azure.svg" style="max-width: 20px;"/> <a href="https://www.credly.com/badges/365eb03d-10e2-4b0f-8d5f-9ef6432b4c89" target="_blank" rel="noopener noreferrer">Microsoft Azure AI Fundamentals</a>`,
+    ].join('\n'),
+
+    // "Skills": [
+    //   "Programming Languages: ",
+    //   `<img src="/icons/python.svg" style="max-width: 20px;"/> Python, <img src="/icons/c++.svg" style="max-width: 20px;"/> C/C++, <img src="/icons/java.svg" style="max-width: 20px;"/> Java, <img src="/icons/javascript.svg" style="max-width: 20px;"/> JavaScript, <img src="/icons/html.svg" style="max-width: 20px;"/> HTML, <img src="/icons/css.svg" style="max-width: 20px;"/> CSS, <img src="/icons/php.svg" style="max-width: 20px;"/> PHP, <img src="/icons/mysql.svg" style="max-width: 20px;"/> MySQL, <img src="/icons/matlab.svg" style="max-width: 20px;"/> MATLAB`, "",
+    //   "Technologies: ",
+    //     `• AI and Machine Learning: <img src="/icons/Keras.svg" style="max-width: 20px;"/> Keras, <img src="/icons/tensorflow.svg" style="max-width: 20px;"/> TensorFlow, <img src="/icons/scikit-learn.svg" style="max-width: 20px;"/> Scikit-Learn, <img src="/icons/OpenCV.svg" style="max-width: 20px;"/> OpenCV`,
+    //     `• Web Development: <img src="/icons/supabase.svg" style="max-width: 20px;"/> Supabase, <img src="/icons/Firebase.svg" style="max-width: 20px;"/> Firebase, <img src="/icons/Vite.svg" style="max-width: 20px;"/> Vite, <img src="/icons/Flask.svg" style="max-width: 20px;"/> Flask, <img src="/icons/React.svg" style="max-width: 20px;"/> ReactJS, <img src="/icons/Node.svg" style="max-width: 20px;"/> Node.js, <img src="/icons/Bootstrap.svg" style="max-width: 20px;"/> Bootstrap, <img src="/icons/Tailwind.svg" style="max-width: 20px;"/> Tailwind CSS, <img src="/icons/Threejs.svg" style="max-width: 20px;"/> ThreeJS`,
+    //     `• Data and Automation: <img src="/icons/NumPy.svg" style="max-width: 20px;"/> Numpy, <img src="/icons/Pandas.svg" style="max-width: 20px;"/> Pandas, <img src="/icons/beautifulsoup.svg" style="max-width: 20px;"/> BeautifulSoup, <img src="/icons/Selenium.svg" style="max-width: 20px;"/> Selenium`,
+    //     "",
+    //   "Developer Tools: ",
+    //   `<img src="/icons/GitHub.svg" style="max-width: 20px;"/> Git, <img src="/icons/Jira.svg" style="max-width: 20px;"/> Jira, <img src="/icons/Azure.svg" style="max-width: 20px;"/> MS Azure, <img src="/icons/powerbi.svg" style="max-width: 20px;"/> MS PowerBI, <img src="/icons/vscode.svg" style="max-width: 20px;"/> VS Code, <img src="/icons/IntelliJ.svg" style="max-width: 20px;"/> Intellij IDEA, <img src="/icons/PhpStorm.svg" style="max-width: 20px;"/> PhpStorm, <img src="/icons/mysql.svg" style="max-width: 20px;"/> MySQL Workbench`, "",
+    //   "Certifications: ",
+    //   "Microsoft Azure Fundamentals, Microsoft Azure AI Fundamentals"
+    // ].join('\n'),
   }
 
-  const handleButtonClick = (buttonName) => {
+  // const handleButtonClick = (buttonName) => {
+  //   setActivePanel(buttonName)
+  // }
+
+  const handleSidebarClick = (buttonName) => {
+    if (buttonName === "Skills") {
+      setActivePanel(null)
+      setActiveButtonPanel(buttonName)
+    } else {
+      setActiveButtonPanel(null)
+      setActivePanel(buttonName)
+    }
+  }
+
+  const handleButtonPanelClick = (buttonName) => {
     setActivePanel(buttonName)
   }
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Escape') {
+        if (activePanel) {
+          setActivePanel(null);
+        } else if (activeButtonPanel) {
+          setActiveButtonPanel(null);
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [activePanel, activeButtonPanel]);
+
   return (
     <>
-      <PageTemplate 
+      {/* <PageTemplate 
         title="About Me"
         sidebars={["Me", "Skills"]}
         onSidebarClick={handleButtonClick}
         footer="About Me"
-      />
+      /> */}
+
+      <PageTemplate 
+        title="About Me"
+        sidebars={activeButtonPanel ? [] : ["Me", "Skills"]}
+        onSidebarClick={handleSidebarClick}
+        footer="About Me"
+        subtitle={activeButtonPanel ? " - " + activeButtonPanel : null}
+      ></PageTemplate>
+
+      {activeButtonPanel && (
+        <ButtonPanel buttons={selections[activeButtonPanel]} onButtonClick={handleButtonPanelClick}></ButtonPanel>
+      )}
 
       {activePanel && (
         <Panel 
--- src/pages/Interests.jsx
+++ src/pages/Interests.jsx
@@ -7,9 +7,57 @@ export default function Interests() {
   const [activePanel, setActivePanel] = useState(null)
   
   const panelContent = {
-    "Art": ["Art? Where?"].join('\n'),
-    "Reading": ["Reading? Reading."].join('\n'),
-    "Music": ["anana"].join('\n'),
+    "Art": ["Art? Where? I'm currently learning and working on it...", "",
+      "In the meanwhile, here's some of my favorite artists:",
+      `• <a href="https://x.com/chohui94" target="_blank" rel="noopener noreferrer">chohui94</a>`,
+      `• <a href="https://x.com/minnnnnnnim" target="_blank" rel="noopener noreferrer">minnnnnnnim</a>`,
+      `• <a href="https://x.com/kieed23232" target="_blank" rel="noopener noreferrer">kieed23232</a>`,
+      `• <a href="https://x.com/john_kafka02" target="_blank" rel="noopener noreferrer">john_kafka02</a>`,
+      `• <a href="https://x.com/Dino_illus" target="_blank" rel="noopener noreferrer">Dino_illus</a>`,
+      `• <a href="https://x.com/REDUM4" target="_blank" rel="noopener noreferrer">REDUM4</a>`,
+      `• <a href="https://x.com/greypidjun" target="_blank" rel="noopener noreferrer">greypidjun</a>`,
+      // `• : <a href="" target="_blank" rel="noopener noreferrer"></a>`,
+    ].join('\n'),
+    "Gaming": [
+      // "Gaming has always been one of my biggest hobbies and passions and is one of the main reasons as to how I got interested in persuing software. Growing up, Minecraft was one of my favorite games and my biggest entry into thinking about how circuits and programming worked. I used to do a lot of redstone contraptions and random command block ideas alongside playing with a bunch of mods and this got me interested in learning more about circuits and how mods were made. I always found it really funny whenever we would get introduced to certain topics in classes and I would realize that I've heard a lot of these terms like gates and latches before in redstone tutorials. ", "",
+      // "My favorite genres are RPGs and Roguelikes of which, one large aspect of these sorts of games is generally learning about enemies and their various AI/patterns which has always been one of my favorite parts about these types of games and what sparked my interest in AI. Overall, this hobby in gaming is what inspired my interest in AI, machine learning, and programming in general and as a result, the main reason why I wanted to pursue Computer Engineering at the University of Waterloo as I thought it would be a good opportunity to gain experience in such things both in class and through coops.", "",
+      // "Gaming has also been a big influence on my other interests, such as art and the sort of music I listen to, with a large number of music I listen to being soundtracks from games like Arknights, Nier Automata, and Stellar Blade.", "",
+      
+      "Gaming has always been one of my biggest hobbies and passions and is one of the main reasons as to how I got interested in persuing software.", "",
+      "Some of my favorite games are:",
+      `• <a href="https://www.arknights.global/" target="_blank" rel="noopener noreferrer">Arknights</a>`,
+      `• <a href="https://www.supergiantgames.com/games/hades/" target="_blank" rel="noopener noreferrer">Hades</a>`,
+      `• <a href="https://www.minecraft.net/" target="_blank" rel="noopener noreferrer">Minecraft</a>`,
+      `• <a href="https://www.niergame.com/en/" target="_blank" rel="noopener noreferrer">Nier Automata</a>`,
+      `• <a href="https://www.pathofexile.com/" target="_blank" rel="noopener noreferrer">Path of Exile</a>`,
+      `• <a href="https://terraria.org/" target="_blank" rel="noopener noreferrer">Terraria</a>`,
+    ].join('\n'),
+    "Reading": [
+      "Reading? Reading. I enjoy reading manga and light nodels.", "", 
+      "Some of my recent favorites are:",
+      // `• Apothecary Diaries`,
+      // `• The Ancient Magus' Bride`,
+      // `• Blue Box`,
+      // `• Delicious in Dungeon`,
+      // `• Frieren: Beyond Journey's End`,
+      // `• The Girl From the Other Side: Siúil, a Rún`,
+      `• <img src="/images/ApothecaryDiaries.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/99026/Kusuriya-no-Hitorigoto/" target="_blank" rel="noopener noreferrer">Apothecary Diaries</a>`,
+      `• <img src="/images/MagusBride.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/85435/Mahoutsukai-no-Yome/" target="_blank" rel="noopener noreferrer">The Ancient Magus' Bride</a>`,
+      `• <img src="/images/BlueBox.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/132182/Ao-no-Hako/" target="_blank" rel="noopener noreferrer">Blue Box</a>`,
+      `• <img src="/images/DungeonMeshi.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/86082/Dungeon-Meshi/" target="_blank" rel="noopener noreferrer">Delicious in Dungeon</a>`,
+      `• <img src="/images/Frieren.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/118586/Frieren-Beyond-Journeys-End/" target="_blank" rel="noopener noreferrer">Frieren: Beyond Journey's End</a>`,
+      `• <img src="/images/OtherSide.jpg" style="max-width: 20px;"/> <a href="https://anilist.co/manga/86770/Totsukuni-no-Shoujo/" target="_blank" rel="noopener noreferrer">The Girl From the Other Side: Siúil, a Rún</a>`,
+
+      // `• : <a href="" target="_blank" rel="noopener noreferrer"></a>`,
+    ].join('\n'),
+    "Music": ["I enjoy listening to some beeps and boops and am looking to get into music production for fun.", "",
+      "In the meanwhile, here's some of my favorite songs right now:",
+      `• <a href="https://www.youtube.com/watch?v=UXV04W0U88k" target="_blank" rel="noopener noreferrer">Arknights EP - Believed Believes Believing</a>`,
+      `• <a href="https://www.youtube.com/watch?v=yaE-Yk6OBfM" target="_blank" rel="noopener noreferrer">Arknights - Battleplan Extinguished Sins</a>`,
+      `• <a href="https://www.youtube.com/watch?v=gpg2k7qGUDg" target="_blank" rel="noopener noreferrer">Stellar Blade - Silent Street (Type A)</a>`,
+      `• <a href="https://www.youtube.com/watch?v=PLG2Uexyi9s" target="_blank" rel="noopener noreferrer">ZUTOMAYO - Hippocampal Pain</a>`,
+      `• <a href="https://www.youtube.com/watch?v=E8RMWLoAsa0" target="_blank" rel="noopener noreferrer">ZUTOMAYO - Blues in the Closet</a>`,
+    ].join('\n'),
   }
 
   const handleButtonClick = (buttonName) => {
@@ -20,7 +68,7 @@ export default function Interests() {
     <>
       <PageTemplate 
         title="Interests"
-        sidebars={["Art", "Reading", "Music"]}
+        sidebars={["Art", "Gaming", "Reading", "Music"]}
         onSidebarClick={handleButtonClick}
         footer="Some of my interests"
       ></PageTemplate>
