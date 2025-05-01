ddcaf374ce6605083132a1ffe830371755d1a490
William <w553li@uwaterloo.ca>
2025-04-30 20:15:22 -0600

    April 30 NVM WE GO BACK

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,32 +13,32 @@ export default function Projects() {
   }
 
   const panelContent = {
-    "ASTunnel":       ['<img src="/w553li.github.io/images/ASTunnel.png" alt="ASTunnel Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "ASTunnel":       ['<img src="/NierPortfolioV2/images/ASTunnel.png" alt="ASTunnel Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: HTML, CSS, JavaScript, ReactJS, React Bootstrap, TensorFlowJS, ThreeJS", 
                       "",
                       "An interactive web game utilizing TensorFlowJS to capture gestures from the user's camera, specfically those resembling American Sign Langauge (ASL), to get points while travelling through a 3D tunnel built with ThreeJS."
     ].join('\n'),
-    "BudgetTracker":  ['<img src="/w553li.github.io/images/BudgetTracker.png" alt="Budget Tracker Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "BudgetTracker":  ['<img src="/NierPortfolioV2/images/BudgetTracker.png" alt="Budget Tracker Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: HTML, CSS, JavaScript, ReactJS, React Bootstrap, Supabase", 
                       "",
                       "A full stack budget tracker built with a React frontend and a Supabase backend. Features custom hooks to allow for full CRUD functionality, such as creating, updating, and removing budgets and expenses. Also features authentication using email, allowing for access to the your budgets across browsers and devices easily."
     ].join('\n'),
-    "ImageSearcher":  ['<img src="/w553li.github.io/images/ImageSearch.png" alt="Image Seacher Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "ImageSearcher":  ['<img src="/NierPortfolioV2/images/ImageSearch.png" alt="Image Seacher Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: HTML, CSS, JavaScript, Flask, Python, Bootstrap", 
                       "",
                       "A full stack image searcher similar to reverse image searching, allowing for the user to input links to images and receive similar images with data such as dimensions and creator through the use of various APIs. Features authentication using email."
     ].join('\n'),
-    "ToDoList":       ['<img src="/w553li.github.io/images/Newtodolist.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "ToDoList":       ['<img src="/NierPortfolioV2/images/Newtodolist.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: HTML, CSS, JavaScript, FireBase", 
                       "",
                       "A full stack ToDoList built with FireBase backend. Features custom hooks to allow for full CRUD functionality, such as creating and deleting ToDoList items."
     ].join('\n'),
-    "Snake":          ['<img src="/w553li.github.io/images/snake.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "Snake":          ['<img src="/NierPortfolioV2/images/snake.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: HTML, CSS, JavaScript", 
                       "",
                       "A web recreation of the popular Snake game with a few additions such as speed functions that work in real time to change the speed of the snake, player score, and various other controls."
     ].join('\n'),
-    "Candlelight":    ['<img src="/w553li.github.io/images/candlelight.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
+    "Candlelight":    ['<img src="/NierPortfolioV2/images/candlelight.png" alt="ToDoList Preview" style="width: 100%; max-width: 600px; margin: 1rem 0;"/>',
                       "Languages and Frameworks: Java", 
                       "",
                       "A roguelike game developed using Java, in which the player explores and endless, randomized dungeon, featuring mechanics such as the randomizing of 10 different stages alongside enemies and items to allow for a unique play-through every single time. The enemies themselves have different stats such as there being faster, but weaker enemies whereas others are much slower but hit much harder. In addition, every 5 stages features 1 of 2 bosses, each with unique abilities and a real-time updating health bar and will also drop a powerful item upon defeat. The player has many different stats such as movement speed, physical and magic damage, and defense, all of which can be increased using the various items scattered around the stages."
